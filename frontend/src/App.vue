ab7030e429765c42965759a486ef33206f25d3d1
Isaac George <i.d.george@hotmail.com>
2024-07-16 14:59:35 +0100

    feat: create frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container class="d-flex fill-height justify-center">
+        <router-view />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts" setup>
+//
+</script>
---
+++ frontend/src/pages/login.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-flex class="w-50">
+    <v-card-title class="text-h4">Login</v-card-title>
+    <v-form>
+      <v-text-field lable="Email" type="email"></v-text-field>
+      <v-text-field label="Password" type="password"></v-text-field>
+      <v-btn type="submit" block>Login</v-btn>
+    </v-form>
+  </v-flex>
+</template>
+<script lang="ts" setup>
+//
+</script>
